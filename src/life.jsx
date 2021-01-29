90bb941601ca4e766eda8bcfc9cd85d106e51b3b
Nikolai <npv3s@skopa.dev>
2021-01-29 17:46:51 +0300

    Работает выбор ширины и высоты

--- src/life.jsx
+++ src/life.jsx
@@ -20,14 +20,16 @@ class Cell extends React.Component {
 class Game extends React.Component {
     constructor(props) {
         super(props);
-        this.height = props.height;
-        this.width = props.width;
+        this.height = 10;
+        this.width = 10;
         this.state = {
             started: false,
             board: Array.from(Array(this.height), () => new Array(this.width).fill(0))
         }
         this.start = this.start.bind(this);
         this.stop = this.stop.bind(this);
+        this.reset = this.reset.bind(this);
+        this.resize = this.resize.bind(this);
         this.update = this.update.bind(this);
         this.onClick = this.onClick.bind(this);
     }
@@ -43,6 +45,22 @@ class Game extends React.Component {
         this.setState({
             started: false
         })
+    }
+
+    reset() {
+        this.stop();
+        this.setState({
+            board: Array.from(Array(this.height), () => new Array(this.width).fill(0))
+        })
+    }
+
+    resize() {
+        let width = document.getElementById("board-width").value;
+        let height = document.getElementById("board-height").value;
+        this.width = (width === "") ? 10 : parseInt(width);
+        this.height = (height === "") ? 10 : parseInt(height);
+        console.log(height, width);
+        this.reset();
     }
 
     componentDidMount() {
@@ -87,23 +105,54 @@ class Game extends React.Component {
 
     render() {
         let rows = [];
-        for (let r = 0; r < this.props.height; r++) {
+        for (let r = 0; r < this.height; r++) {
             let row = [];
-            for (let c = 0; c < this.props.width; c++)
-                row.push(<Cell row={r} column={c}
-                               value={this.state.board[r][c]}
-                               onClick={this.onClick}/>);
+            for (let c = 0; c < this.width; c++) {
+                if (this.state.started)
+                    row.push(<Cell row={r} column={c}
+                                   value={this.state.board[r][c]}/>);
+                else
+                    row.push(<Cell row={r} column={c}
+                                   value={this.state.board[r][c]}
+                                   onClick={this.onClick}/>);
+            }
             rows.push(<div className="row" key={String(r)}>{row}</div>);
         }
 
         return (
-            <>
-                <div className="board">
-                    {rows}
+            <div className="columns">
+                <div className="column is-narrow">
+                    <div className="board">
+                        {rows}
+                    </div>
                 </div>
-                {(this.state.started) ? <button onClick={this.stop}>Stop</button> :
-                    <button onClick={this.start}>Start</button>}
-            </>
+                <div className="column">
+                    <div className="field">
+                        <div className="label">Размер</div>
+                        <div className="field is-grouped sizes">
+                            <div className="control">
+                                <input id="board-height" className="input" type="text" placeholder="Высота"/>
+                            </div>
+                            <div className="control">
+                                <input id="board-width" className="input" type="text" placeholder="Ширина"/>
+                            </div>
+                            <div className="control">
+                                <button onClick={this.resize} className="button">Применить</button>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="field is-grouped">
+                        <div className="control">
+                            {(this.state.started) ?
+                                <button onClick={this.stop} className="button is-danger">Стоп</button> :
+                                <button onClick={this.start} className="button is-primary">Старт</button>}
+                        </div>
+                        <div className="control">
+                            <button onClick={this.reset} className="button is-info">Сброс</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
         )
     }
 }
