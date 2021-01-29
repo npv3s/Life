3c424ba87369b59cdc1cb53e0e3b8e9b4babd089
Nikolai <npv3s@skopa.dev>
2021-01-29 12:18:36 +0300

    Теперь с работающим алгоритмом

--- src/life.jsx
+++ src/life.jsx
@@ -12,7 +12,7 @@ class Cell extends React.Component {
         return (
             <div key={String(row) + " " + String(column)}
                  className={"cell" + ((this.props.value === 1) ? " clicked" : "")}
-                 onClick={() => this.props.onUpdate(row, column)}/>
+                 onClick={() => this.props.onClick(row, column)}/>
         )
     }
 }
@@ -20,13 +20,43 @@ class Cell extends React.Component {
 class Game extends React.Component {
     constructor(props) {
         super(props);
+        this.height = props.height;
+        this.width = props.width;
         this.state = {
-            board: Array.from(Array(props.height), () => new Array(props.width).fill(0))
+            board: Array.from(Array(this.height), () => new Array(this.width).fill(0))
         }
-        this.onUpdate = this.onUpdate.bind(this);
+        this.update = this.update.bind(this);
+        this.onClick = this.onClick.bind(this);
     }
 
-    onUpdate(row, column) {
+
+    update() {
+        let b = this.state.board;
+        let u = Array.from(Array(this.height), () => new Array(this.width).fill(0));
+        for (let r = 0; r < this.height; r++) {
+            for (let c = 0; c < this.width; c++) {
+                let sum = 0;
+                for (let i = Math.max(r - 1, 0); i < Math.min(r + 2, this.height); i++) {
+                    for (let y = Math.max(c - 1, 0); y < Math.min(c + 2, this.width); y++) {
+                        //console.log("i:", i, "y:", y, b[i][y])
+                        if ((i !== r) || (y !== c)) sum += b[i][y]
+                    }
+                }
+                console.log(r, c, sum);
+                if (sum === 3)
+                    u[r][c] = 1;
+                else if (sum === 2)
+                    u[r][c] = b[r][c];
+                else
+                    u[r][c] = 0;
+            }
+        }
+        this.setState({
+            board: u
+        })
+    }
+
+    onClick(row, column) {
         console.log(row, column);
         let board = this.state.board;
         board[row][column] = (board[row][column] === 0) ? 1 : 0;
@@ -35,23 +65,30 @@ class Game extends React.Component {
         })
     }
 
+    componentDidMount() {
+        //setInterval(this.update, 1000);
+    }
+
     render() {
         let rows = [];
-        for (let row_index = 0; row_index < this.props.height; row_index++) {
+        for (let r = 0; r < this.props.height; r++) {
             let row = [];
-            for (let column_index = 0; column_index < this.props.width; column_index++)
-                row.push(<Cell row={row_index} column={column_index}
-                               value={this.state.board[row_index][column_index]}
-                               onUpdate={this.onUpdate}/>);
-            rows.push(<div className="row" key={String(row_index)}>{row}</div>);
+            for (let c = 0; c < this.props.width; c++)
+                row.push(<Cell row={r} column={c}
+                               value={this.state.board[r][c]}
+                               onClick={this.onClick}/>);
+            rows.push(<div className="row" key={String(r)}>{row}</div>);
         }
 
         return (
-            <div className="board">
-                {rows}
-            </div>
+            <>
+                <div className="board">
+                    {rows}
+                </div>
+                <button onClick={this.update}>Start</button>
+            </>
         )
     }
 }
 
-ReactDOM.render(<Game height = {10} width = {10}/>, document.querySelector(".game"));
+ReactDOM.render(<Game height={10} width={10}/>, document.querySelector(".game"));
